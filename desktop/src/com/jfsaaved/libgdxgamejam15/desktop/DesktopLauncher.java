e9f711482ad488f08d147b0d303ed0a8f35489fd
Julian Saavedra <julian.felipe.saavedra@gmail.com>
2015-12-30 20:56:16 -0500

    First commit

---
+++ desktop/src/com/jfsaaved/libgdxgamejam15/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jfsaaved.libgdxgamejam15.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jfsaaved.libgdxgamejam15.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
